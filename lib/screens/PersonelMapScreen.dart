dc241c4949745a30f70398904799937dc08237e3
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-02-22 11:45:32 +0300

    Goole Maps

--- lib/screens/PersonelMapScreen.dart
+++ lib/screens/PersonelMapScreen.dart
@@ -9,11 +9,24 @@ class PersonelMapScreen extends StatefulWidget {
 }
 
 class _PersonelMapScreenState extends State<PersonelMapScreen> {
+  GoogleMapController? _controller = null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Personel Harita")),
-      body: GoogleMap(),
+      body: GoogleMap(
+        myLocationButtonEnabled: true,
+        mapType: MapType.hybrid,
+        initialCameraPosition:
+            CameraPosition(target: LatLng(40.188526, 29.060965), zoom: 15),
+        compassEnabled: true,
+        scrollGesturesEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+      ),
     );
   }
 }
