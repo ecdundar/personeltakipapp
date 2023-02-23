a0490c0ed5656507c369a56daba484610829f6eb
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-02-23 10:23:38 +0300

    Second Marker

--- lib/screens/PersonelMapScreen.dart
+++ lib/screens/PersonelMapScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PersonelMapScreen extends StatefulWidget {
   const PersonelMapScreen({super.key});
@@ -10,6 +12,7 @@ class PersonelMapScreen extends StatefulWidget {
 
 class _PersonelMapScreenState extends State<PersonelMapScreen> {
   GoogleMapController? _controller = null;
+  Position? currentPosition = null;
 
   Set<Marker> getMarkers() {
     var _marker = Marker(
@@ -21,11 +24,51 @@ class _PersonelMapScreenState extends State<PersonelMapScreen> {
         ?.animateCamera(CameraUpdate.newLatLngZoom(_marker.position, 15));
     //Kamerayı move etme
     //_controller.moveCamera(CameraUpdate.newLatLngZoom(latLng, zoom))
-    return {_marker};
+
+    if (currentPosition == null) {
+      return {_marker};
+    } else {
+      var currentMarker = Marker(
+          markerId: MarkerId("Marker-Current"),
+          position:
+              LatLng(currentPosition!.latitude, currentPosition!.longitude),
+          infoWindow: InfoWindow(title: "Konumum"));
+      return {_marker, currentMarker};
+    }
+  }
+
+  void getLocation(BuildContext context) async {
+    bool isServiceEnabled = false;
+    LocationPermission permission;
+
+    //Konum Servisleri Açık Mı Değil Mi ?
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Lütfen konum servislerini aktif ediniz.')));
+      return;
+    }
+
+    //Konum servisleri açık, uygulamanın yetkisi yok.
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text('Uygulamaya konum servislerine erişim yetkisi veriniz.')));
+        openAppSettings();
+      }
+    }
+
+    currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    getLocation(context);
     return Scaffold(
       appBar: AppBar(title: Text("Personel Harita")),
       body: GoogleMap(
